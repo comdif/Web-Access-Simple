90bb421f22bed5eb4d5dee9347c7e4364f0f297c
Christian <142540893+comdif@users.noreply.github.com>
2025-10-31 09:11:34 +0100

    better structure and functionality

--- html/header.php
+++ html/header.php
@@ -1,65 +1,53 @@
 <?php
-//print_r($_COOKIE); 
-echo'<head>';
-echo'<link rel="stylesheet" href="header.css">';
-?><script>
-function eyeon()
-	{
-	var x = document.getElementById("mypwrd");
-	if (x.type === "password")
-		{ x.type = "text"; }
-	else
-		{ x.type = "password"; }
-	}
-function eyeout()
-	{
-	var x = document.getElementById("mypwrd");
-	if (x.type === "text")
-		{ x.type = "password"; }
-	else
-		{ x.type = "text"; }
-	}
-function eyeon2()
-	{
-	var x = document.getElementById("mypwrd2");
-	if (x.type === "password")
-		{ x.type = "text"; }
-	else
-		{ x.type = "password"; }
-	}
-function eyeout2()
-	{
-	var x = document.getElementById("mypwrd2");
-	if (x.type === "text")
-		{ x.type = "password"; }
-	else
-		{ x.type = "text"; }
-	}
-</script>
+// Redirect if not authenticated (before any output)
+if (!isset($_COOKIE['admin']) && !isset($_COOKIE['user'])) {
+    header("Location: index.php");
+    exit;
+}
+
+// HTML head output
+?>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="header.css">
+    <script>
+        // Toggle password visibility for #mypwrd and #mypwrd2
+        function togglePassword(id) {
+            var x = document.getElementById(id);
+            if (!x) return;
+            x.type = (x.type === "password") ? "text" : "password";
+        }
+    </script>
+</head>
 <?php
-echo'</head>';
-function checkpw($str){ 
-		return 
-		preg_match('/[a-z]/', $str)
-		&& preg_match('/[A-Z]/', $str)
-		&& preg_match('/[%*_\-]/', $str) 
-		&& preg_match('/[0-9]/', $str); 
-		} 
-if(!isset($_COOKIE['admin']) && !isset($_COOKIE['user'])){ header("Location: index.php"); }
-else
-	{
-	////////////////OPEN THE DATABASE FILE
-	$base = '../dbdir/mybase.sqlite';
-	$con = new SQLite3($base);
-	if(!$con)
-		{
-		echo $db->lastErrorMsg(); exit(0);
-		}
-echo'<div class="mybody">';
-	/////////// HEADER HERE ///////////
-	echo'<p><a href="content.php"><font style="font-size: 10px;color: white;">
-	Home</a> &nbsp; &nbsp; &nbsp; <a href="account.php?pwadm=1">My Account</a> &nbsp; &nbsp; &nbsp; <a href="account.php?logout=1">Logout</font></a></p>';
-	/////////// END HEADER ///////////
-	echo'</div><br/>';	
-	}	
+
+// Password policy: at least 1 lowercase, 1 uppercase, 1 number, and 1 special char (%*-_)
+function checkpw($str) {
+    return
+        preg_match('/[a-z]/', $str) &&
+        preg_match('/[A-Z]/', $str) &&
+        preg_match('/[%*_\-]/', $str) &&
+        preg_match('/[0-9]/', $str);
+}
+
+// Open the database (with error handling)
+$base = '../dbdir/mybase.sqlite';
+$con = new SQLite3($base);
+if (!$con) {
+    die("Database connection failed: " . $con->lastErrorMsg());
+}
+
+// Output header
+?>
+<div class="mybody">
+    <p>
+        <a href="content.php" style="font-size: 10px; color: white;">Home</a>
+        &nbsp; &nbsp; &nbsp;
+        <a href="account.php?pwadm=1" style="font-size: 10px; color: white;">My Account</a>
+        &nbsp; &nbsp; &nbsp;
+        <a href="account.php?logout=1" style="font-size: 10px; color: white;">Logout</a>
+    </p>
+</div>
+<br/>
+<?php
 ?>
